38fb60df7af9a42b579c8f11b38b4c74ed9ecd8e
flads <flads.me@gmail.com>
2021-09-05 22:21:09 -0300

    feat - install click and create command group

--- tfaker.py
+++ tfaker.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
 
+import click
 import pyperclip
 import random
 
+def generateCPF():
+    firstSet = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    secondSet = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+    cpf = [random.randint(1, 9) for _ in range(9)]
+
+    # Calculate first verifying digit
+    firstResultSumValues = 0
+
+    for x in range(len(cpf)):
+        firstResultSumValues += cpf[x] * firstSet[x]
+
+    firstVerifyingDigit = 11 - (11 if (firstResultSumValues % 11) < 2 else (firstResultSumValues % 11))
+
+    cpf.append(firstVerifyingDigit)
+
+    # Calculate second verifying digit
+    secondResultSumValues = 0
+
+    for x in range(len(cpf)):
+        secondResultSumValues += cpf[x] * secondSet[x]
+
+    secondVerifyingDigit = 11 - (11 if (secondResultSumValues % 11) < 2 else (secondResultSumValues % 11))
+
+    cpf.append(secondVerifyingDigit)
+
+    cpf = ''.join(str(x) for x in cpf)
+
+    pyperclip.copy(cpf)
+    pyperclip.paste()
+
 def generateCNPJ():
-    firstSet = [5 ,4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    firstSet = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
     secondSet = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
     cnpj = [random.randint(1, 9) for _ in range(12)]
@@ -34,7 +66,22 @@ def generateCNPJ():
     pyperclip.copy(cnpj)
     pyperclip.paste()
 
-    print('CNPJ copied!')
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def cpf():
+    generateCPF()
+    click.echo(click.style('CPF copied!', bg='green', fg='black'))
+
+@click.command()
+def cnpj():
+    generateCNPJ()
+    click.echo(click.style('CNPJ copied!', bg='green', fg='black'))
+
+cli.add_command(cpf)
+cli.add_command(cnpj)
 
 if __name__ == '__main__':
-    generateCNPJ()
+    cli()
