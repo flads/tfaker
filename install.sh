414119b6618f9ddab2f474a40985e8b79da5a467
flads <flads.me@gmail.com>
2021-09-06 00:38:11 -0300

    fix - remove --help from install script

--- install.sh
+++ install.sh
@@ -7,5 +7,3 @@ mkdir -p ~/src && git clone git@github.com:flads/tfaker.git ~/src/tfaker
 cd ~/src/tfaker && pip3 install -r requirements.txt
 
 sudo ln -s ~/src/tfaker/tfaker.py /usr/local/bin/tf
-
-tf --help
