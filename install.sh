6ec2ddec80e059d1767a3bd2e200c512a393ceff
flads <flads.me@gmail.com>
2021-09-06 00:33:53 -0300

    feat - add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install python3-pip -y
+
+mkdir -p ~/src && git clone git@github.com:flads/tfaker.git ~/src/tfaker
+
+cd ~/src/tfaker && pip3 install -r requirements.txt
+
+sudo ln -s ~/src/tfaker/tfaker.py /usr/local/bin/tf
+
+tf --help
